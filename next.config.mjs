912dbb0831f3dff2d4570928aa032e679ec59814
sarthak pokhrel <sarthakpokhrel@Mac-3695.lan>
2025-09-20 14:04:59 -0400

    updated files again

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
-}
-export default nextConfig
+  basePath: "/portfolio",       // your repo name
+  assetPrefix: "/portfolio/",   // ensures CSS/JS assets load
+};
+
+export default nextConfig;
